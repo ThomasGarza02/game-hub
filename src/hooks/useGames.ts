ac64f4d92710f2601317b88707231d17b1ac91ff
ThomasGarza02 <thomas.garza02@utrgv.edu>
2025-06-26 11:27:24 -0500

    handle game with no image

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenre";
+
 
 export interface Platform{
   id: number
@@ -22,7 +22,11 @@ export interface Game {
   const useGames = 
   (gameQuery:GameQuery) => 
     useData<Game>('/games',{
-      params:{genres:gameQuery.genre?.id,platforms:gameQuery.platform?.id}}, 
+      params:{
+        genres:gameQuery.genre?.id,
+        platforms:gameQuery.platform?.id,
+        ordering:gameQuery.sortOrder
+      }}, 
       [gameQuery]);
 
 export default useGames;
